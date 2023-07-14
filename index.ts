b7fbad03f26dc071f7cd2edd2445fbb9b497e54e
trenkenshu <trenkenshu@gmail.com>
2023-07-14 13:34:22 +0300

    feat: ws connect

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
 import { httpServer } from "./src/http_server/index"
+import createServer from "./src/ws_server"
 
 const HTTP_PORT = 8181
 const WS_PORT = 3000
-console.log(`Start static http server on the ${HTTP_PORT} port!`)
-httpServer.listen(HTTP_PORT)
+
+httpServer.listen(
+  HTTP_PORT,
+  () => console.log(`Start static http server on the ${HTTP_PORT} port!`)
+)
+createServer(
+  WS_PORT,
+  () => console.log(`Start ws server on the ${WS_PORT} port!`)
+)
+
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,14 +1,18 @@
 import WebSocket from 'ws'
 import { wsEvents } from './types'
 
-const createServer = (port) => {
+const createServer = (port: number, cb: () => void) => {
   const server = new WebSocket.Server({
   port
-})
+}, cb)
 
 server.on(wsEvents.connection, client => {
   client.on(wsEvents.message, message => {
     return
+  })
+
+  client.on(wsEvents.error, error => {
+    console.log(`We've got error: ${error.message}`)
   })
 })
 
